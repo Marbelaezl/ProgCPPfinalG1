2c2a0f546d8369b68e23ab147b0b2fefb9b1f50a
DBarrera17 <150556671+DBarrera17@users.noreply.github.com>
2023-11-14 17:42:30 -0500

    Add files via upload

---
+++ twodim.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <cmath>
+#include <vector>
+#include <random>
+
+void newposition(std::vector<double> &position, double angle );
+void randanggen(std::vector<double> &theta, int seed);
