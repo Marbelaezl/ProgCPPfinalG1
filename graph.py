0722a62eafd96131517ea6f5b0d0d12a9093fec2
Marbelaezl <124514421+Marbelaezl@users.noreply.github.com>
2023-11-23 17:42:53 -0500

    Fixed error in graphing sequence

--- graph.py
+++ graph.py
@@ -14,7 +14,7 @@ ax[0].set_title("seed = 1")
 ax[1].set_title("seed=2")
 plt.show()
 
-dist=np.genfromtxt("longitudes.txt")
+dist=np.genfromtxt("longitud.txt")
 fig_len,ax_len=plt.subplots()
 fig_stable,ax_stable=plt.subplots()
 
@@ -24,13 +24,15 @@ ax_len.set_title("Distancia promedio para N = 500 monómeros")
 
 ax_stable.set_xlabel("unstable positions")
 ax_stable.set_ylabel("counts")
-ax_stable.set_title("Posiciones inestables para N = 500 monómeros")
+ax_stable.set_title("Pares inestables para N = 500 monómeros")
 print(dist)
+
+colors=["red","black","green"]
 for i in range(0,3):
     avglen= np.mean(dist[:,2*i +1])
     avgstable=np.mean(dist[:,2*i+2])
-    ax_len.hist(dist[:,2*i+1], bins=20,label ="case " + str(i+2) +": "  + str(avglen.round(3)))
-    ax_stable.hist(dist[:,2*i+1], bins=20,label ="case " + str(i+2) +": "  + str(int(avgstable)))
+    ax_len.hist(dist[:,2*i+1], bins=20,label ="case " + str(i+1) +": "  + str(avglen.round(3)),alpha=0.6,color=colors[i])
+    ax_stable.hist(dist[:,2*i+2], bins=20,label ="case " + str(i+1) +": "  + str(int(avgstable)),alpha=0.6,color=colors[i])
                 
     
 
